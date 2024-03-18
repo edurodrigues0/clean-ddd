a0b16f7c57cb90b6d2bfdb2ebc5d2fedeeef94ff
edurodrigues0 <edusacramento9@gmail.com>
2024-03-18 20:27:18 -0300

    Fórum com DDD & Clean Architecture - Caso de uso: Editar pergunta

---
+++ src/domain/forum/application/use-cases/edit-question.spec.ts
@@ -0,0 +1,57 @@
+import { InMemoryQuestionRepository } from 'test/repositories/in-memory-questions-repository'
+import { makeQuestion } from 'test/factories/make-question'
+import { UniqueEntityID } from '@/core/entities/unique-entity-id'
+import { EditQuestionUseCase } from './edit-question'
+
+let inMemoryQuestionsRepository: InMemoryQuestionRepository
+let sut: EditQuestionUseCase
+
+describe('Edit Question', () => {
+  beforeEach(() => {
+    inMemoryQuestionsRepository = new InMemoryQuestionRepository()
+    sut = new EditQuestionUseCase(inMemoryQuestionsRepository)
+  })
+
+  it('should be able to edit a question', async () => {
+    const newQuestion = makeQuestion(
+      {
+        authorId: new UniqueEntityID('author-01'),
+      },
+      new UniqueEntityID('question-01'),
+    )
+
+    await inMemoryQuestionsRepository.create(newQuestion)
+
+    await sut.execute({
+      questionId: newQuestion.id.toString(),
+      authorId: 'author-01',
+      title: 'Título editado',
+      content: 'Conteúdo editado',
+    })
+
+    expect(inMemoryQuestionsRepository.items[0]).toMatchObject({
+      title: 'Título editado',
+      content: 'Conteúdo editado',
+    })
+  })
+
+  it('should not be able to edit question from another user', async () => {
+    const newQuestion = makeQuestion(
+      {
+        authorId: new UniqueEntityID('author-01'),
+      },
+      new UniqueEntityID('question-01'),
+    )
+
+    await inMemoryQuestionsRepository.create(newQuestion)
+
+    await expect(async () => {
+      await sut.execute({
+        authorId: 'autor-diferente',
+        questionId: newQuestion.id.toValue(),
+        title: 'Título editado',
+        content: 'Conteúdo editado',
+      })
+    }).rejects.toThrow('Not allowed.')
+  })
+})
--- test/repositories/in-memory-questions-repository.ts
+++ test/repositories/in-memory-questions-repository.ts
@@ -8,6 +8,12 @@ export class InMemoryQuestionRepository implements QuestionsRepository {
     this.items.push(question)
   }
 
+  async save(question: Question) {
+    const itemIndex = this.items.findIndex((item) => item.id === question.id)
+
+    this.items[itemIndex] = question
+  }
+
   async findBySlug(slug: string) {
     const question = this.items.find((item) => item.slug.value === slug)
 
