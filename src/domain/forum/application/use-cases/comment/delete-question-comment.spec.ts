688ea0f05bf507d4bd8896f377c4bf7552c45e90
edurodrigues0 <edusacramento9@gmail.com>
2024-03-19 16:29:30 -0300

    Fórum com DDD & Clean Architecture - Caso de uso: Deletar comentário da resposta

--- src/domain/forum/application/use-cases/comment/delete-question-comment.spec.ts
+++ src/domain/forum/application/use-cases/comment/delete-question-comment.spec.ts
@@ -2,7 +2,6 @@ import { InMemoryQuestionCommentsRepository } from 'test/repositories/in-memory-
 import { DeleteQuestionCommentUseCase } from './delete-question-comment'
 import { makeQuestionComment } from 'test/factories/make-question-comment'
 import { UniqueEntityID } from '@/core/entities/unique-entity-id'
-import exp from 'constants'
 
 let inMemoryQuestionCommentsRepository: InMemoryQuestionCommentsRepository
 let sut: DeleteQuestionCommentUseCase
--- test/repositories/in-memory-answer-comments-repository.ts
+++ test/repositories/in-memory-answer-comments-repository.ts
@@ -9,4 +9,22 @@ export class InMemoryAnswerCommentsRepository
   async create(answerComments: AnswerComment) {
     this.items.push(answerComments)
   }
+
+  async findById(id: string) {
+    const answerComment = this.items.find((item) => item.id.toString() === id)
+
+    if (!answerComment) {
+      return null
+    }
+
+    return answerComment
+  }
+
+  async delete(answerComment: AnswerComment) {
+    const itemIndex = this.items.findIndex(
+      (item) => item.id.toString() === answerComment.id.toString(),
+    )
+
+    this.items.splice(itemIndex, 1)
+  }
 }
